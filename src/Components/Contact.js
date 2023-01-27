c8476c459262418abb09add7de06edab87940eb2
Ondeyo Matoke <ondeyostephen30@gmail.com>
2023-01-27 19:29:37 +0300

    css changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Contact() {
   return (
     <div>
-      <h1>my contact</h1>
+      <h1>this where to put contact information</h1>
     </div>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,29 +4,36 @@ import '../App.css'
 
 export default function Navbar() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="about/">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
+    <>   
+   
+      <nav className='navbar'>        
+        
+        <i class="fa-solid fa-bars" className='menu'></i> 
+        <div className='navbar-links'>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="about/">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
 
-          <li>
-            <Link to="/signup" className='saynup'>Create account</Link>
-          </li>
+              <li>
+                <Link to="/signup" className='saynup'>Create account</Link>
+              </li>
 
-          <li>
-            <Link to="/signin" className='saynin'>Sign In</Link>
-          </li>
-          
-        </ul>
+              <li>
+                <Link to="/signin" className='saynin'>Sign In</Link>
+              </li>              
+            </ul>
+            
+            </div>   
+                
       </nav>
-    </div>
+      
+    </>
   )
 }
